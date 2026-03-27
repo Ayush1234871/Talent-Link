5a3c0b149ca691b85bae98d65dcb8119d42d57bd
Ayush <Win@Len>
2026-03-27 09:39:32 +0530

    Add Render deployment configuration and update settings for production

--- backend/talentlink/settings.py
+++ backend/talentlink/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-7exh!cuh3#a-y*%%uxsl!8v2%p9re^pz5ha^#oq4z@k!%#)8$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+import os
+
+ALLOWED_HOSTS = ['*'] # In production, replace with specific Render URL
 
 
 # Application definition
@@ -54,6 +56,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
